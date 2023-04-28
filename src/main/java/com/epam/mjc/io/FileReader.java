0e667bcf9d8734bbce6bc88a28acf55e4e358776
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-28 23:10:19 +0500

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,23 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder str = new StringBuilder();
+        try (FileInputStream fileInputStream = new FileInputStream(file.getAbsoluteFile());) {
+            int c;
+            while ((c = fileInputStream.read()) != -1) {
+                char c1 = (char) c;
+                str.append(c1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile("Anna", 25, "anna@mailserver.com", 1234567890L);
     }
 }
